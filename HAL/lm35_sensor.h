cafdde9911f0e0f34580586ebd56fc7fd60c3abb
SherifBeshr <shefoalaao@gmail.com>
2022-02-20 22:43:08 +0200

    LM35 ( Temperature Sensor ) driver

---
+++ HAL/lm35_sensor.h
@@ -0,0 +1,30 @@
+/******************************************************************************************************
+File Name	: lm35.h
+Author		: Sherif Beshr
+Description : header file for the LM35 Temperature Sensor driver
+*******************************************************************************************************/
+
+#ifndef LM35_SENSOR_H_
+#define LM35_SENSOR_H_
+
+#include "std_types.h"
+
+/***************************************************************************************************
+ *                                		Definitions                                  			   *
+ ***************************************************************************************************/
+
+#define SENSOR_CHANNEL_ID				2
+#define SENSOR_MAX_VOLT_VALUE			1.5
+#define SENSOR_MAX_TEMPERATURE    		150
+
+/***************************************************************************************************
+ *                                	Function Prototype                                  		   *
+ ***************************************************************************************************/
+
+/*
+ * Description :
+ * Function responsible for calculate the temperature from the ADC digital value.
+ */
+uint8 LM35_getTemperature(void);
+
+#endif /* LM35_SENSOR_H_ */
